784000891b9c1d2d48761349ad20cd352b6a64c3
Rachel <rachel.jansen0613@gmail.com>
2021-06-22 09:10:36 -0500

    initial commit, completed assignment

---
+++ chaining_methods.py
@@ -0,0 +1,26 @@
+class User:
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.account_balance = 0
+    def make_deposit(self, amount):	
+        self.account_balance += amount
+        return self
+    def make_withdrawal(self, amount):
+        self.account_balance -= amount
+        return self
+    def display_user_balance(self):
+        print(f"User: {self.name}, Balance: ${self.account_balance}")
+        # return self
+
+    
+
+mitchell = User("Mitchell Caldwell", "mitchel@python.com")
+kristy = User("Kristy Blair", "kristy@python.com")
+darin = User("Darin James", "darin@python.com")
+
+mitchell.make_deposit(250).make_deposit(550).make_deposit(100).make_withdrawal(200).display_user_balance()
+
+kristy.make_deposit(200).make_deposit(250).make_withdrawal(100).make_withdrawal(175).display_user_balance()
+
+darin.make_deposit(800).make_withdrawal(200).make_withdrawal(300).make_withdrawal(250).display_user_balance()
