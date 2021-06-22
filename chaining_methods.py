136b01bcdb51e5cee9d1b9dce905e778277f06f8
Rachel <rachel.jansen0613@gmail.com>
2021-06-22 09:31:29 -0500

    added bonus step of transfer_money to assignment

--- chaining_methods.py
+++ chaining_methods.py
@@ -12,7 +12,9 @@ class User:
     def display_user_balance(self):
         print(f"User: {self.name}, Balance: ${self.account_balance}")
         # return self
-
+    def transfer_money(from_name, to_name, amount):
+        from_name.make_withdrawal(amount)
+        to_name.make_deposit(amount)
     
 
 mitchell = User("Mitchell Caldwell", "mitchel@python.com")
@@ -24,3 +26,7 @@ mitchell.make_deposit(250).make_deposit(550).make_deposit(100).make_withdrawal(2
 kristy.make_deposit(200).make_deposit(250).make_withdrawal(100).make_withdrawal(175).display_user_balance()
 
 darin.make_deposit(800).make_withdrawal(200).make_withdrawal(300).make_withdrawal(250).display_user_balance()
+
+mitchell.transfer_money(darin, 100)
+mitchell.display_user_balance()
+darin.display_user_balance()
